0f04589efa39ac59f69fc0ed78eee04fe2bed2d3
kan_chan <shunplay1793@gmail.com>
2026-06-21 18:25:29 +0900

    postモデル,userモデルのバリデーションと関連付けを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  # 料理名：空欄NG 外部キー（user）：空欄NG いつ頃：空欄NG エピソード：空欄NG
+  validates :title, presence: true
+  validates :user_id, presence: true
+  validates :when, presence: true
+  validates :episode, presence: true
 end
