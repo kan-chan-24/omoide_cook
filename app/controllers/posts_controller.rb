a0f50f495b06ce0a2059e88460306ed0d29eb5c1
kan_chan <shunplay1793@gmail.com>
2026-06-25 21:08:27 +0900

    モーダル実装の関連ファイルを作成、ディレクトリ配置の修正が必要

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,47 @@
 class PostsController < ApplicationController
   def index
+    # 投稿一覧用：全てのpostsを取得
     @posts = Post.includes(:user).order(created_at: :desc)
+    # モーダルの投稿作成用に裏でpostオブジェクトを持たせる
+    @post = Post.new
+  end
+
+  def create
+    # フォームから送られてきたデータを取得
+    @post = Post.new(post_params)
+
+    # -- バリデーションOK --
+    if @post.save
+      # どういう形式でブラウザから通信が送られてきたかを確認
+      respond_to do |format|
+        # Turbo（非同期）の場合
+        format.turbo_stream do
+          # id="posts"（投稿の配列）の先頭に、今作った新しい投稿（@post）のカードを追加（prepend）する
+          # モーダルのpost_formの中身を新しく作り直した空のフォーム（Post.new）で上書き（update）してリセットする
+          render turbo_stream: [
+            turbo_stream.prepend("posts", partial: "posts/post", locals: { post: @post }),
+            turbo_stream.update("post_form", partial: "posts/form", locals: { post: Post.new })
+          ]
+        end
+        # 通常のページ遷移を伴う通信がきた場合、ページ全体をリダイレクトする（保険的な措置）
+        format.html { redirect_to posts_path }
+      end
+    # -- バリデーションNG --  
+    else
+      respond_to do |format|
+        # Turbo（非同期）の場合
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update("post_form", partial: "posts/form", locals: { post: @post })
+        end
+        # 通常のページ遷移を伴う通信がきた場合は、render処理で画面を繰り返す）
+        format.html { render :index }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :when, :episode, :image)
   end
 end
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -7,17 +7,24 @@ export default class extends Controller {
 
     connect() {
         // 初期状態では非表示
+        console.log("Modal controller connected!")  // ← 追加
+        console.log("containerTarget:", this.containerTarget)  // ← 追加
+        console.log("backdropTarget:", this.backdropTarget)    // ← 追加
+        console.log("contentTarget:", this.contentTarget)      // ← 追加
+
         this.containerTarget.style.display = "none"
     }
 
     open() {
         // モーダルの外枠を表示、背景ページをスクロールさせない
+        console.log("open() メソッドが呼ばれました!")  // ← 追加
         this.containerTarget.style.display = "block"
         document.body.style.overflow = "hidden"
     }
 
     close() {
         // モーダルの外枠を非表示、背景ページのスクロールを元通りに
+        console.log("close() メソッドが呼ばれました!")  // ← 追加
         this.containerTarget.style.display = "none"
         document.body.style.overflow = "auto"
     }
