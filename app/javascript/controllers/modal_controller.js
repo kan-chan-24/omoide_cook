9d2769591badff7eee14181645aa84df6615cd62
kan_chan <shunplay1793@gmail.com>
2026-07-02 16:48:40 +0900

    投稿詳細機能の実装

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -7,16 +7,12 @@ export default class extends Controller {
     connect() {
         // 初期状態では非表示
         console.log("=== Modal Controller Connect ===")
-        console.log("Controller:", this)
-        console.log("================================")
         this.containerTarget.style.display = "none"
     }
 
     open() {
         // モーダルの外枠を表示、背景ページをスクロールさせない
         console.log("=== open() メソッド呼び出し ===")
-        console.log("containerTarget:", this.containerTarget)
-        console.log("================================")
         this.containerTarget.style.display = "block"
         document.body.style.overflow = "hidden"
     }
@@ -26,5 +22,11 @@ export default class extends Controller {
         console.log("=== close() メソッド呼び出し ===")
         this.containerTarget.style.display = "none"
         document.body.style.overflow = "auto"
+
+        const frame = this.element.querySelector("#modal_content")
+        if (frame) {
+            frame.src = null        // 読み込み元のURLをクリア
+            frame.innerHTML = ""    // 枠の中のHTMLを完全に消去
+        }
     }
 }
