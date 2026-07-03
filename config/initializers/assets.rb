e470385e3c885429fd3f287466defaf5e5b287cc
kan_chan <shunplay1793@gmail.com>
2026-07-03 12:33:31 +0900

    JavaScriptを本番環境で読み込ませる設定を追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,9 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Railsがアセット（CSS/JS）を探す対象に、javascriptフォルダを追加します
+Rails.application.config.assets.paths << Rails.root.join("app", "javascript")
+
+# もし将来的に外部ライブラリを使う場合のために以下も追加しておくと安全です
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
